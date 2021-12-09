d0dafaaa2566dae947cba3849ad5916271917264
1Arpit-jaiswal <arpitjaiswal2302@gmail.com>
2021-12-09 22:00:13 +0530

    files added

---
+++ form-handelr.php/form-handler.php
@@ -0,0 +1,23 @@
+<?php
+$name = $_POST[ 'name' ];
+$visitor_email = $_POST[ 'email' ];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+
+$email_from='Univer@sdd.com';
+
+$email_subject='New Form Submission';
+
+$email_body = "User Name: $name.\n".
+               "User Email: $visitor_email.\n".
+               "Subject: $subject.\n".
+               "User Message: $message.\n";
+
+$to='arpitjaiswal2302@gmail.com';
+$headers =    "From: $email_from\r\n";  
+$headers. = "Replay-To: $visitor_email\r\n "; 
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location:conatct.html");
+?>
